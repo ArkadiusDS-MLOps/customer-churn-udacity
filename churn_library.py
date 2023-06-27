bb5d313d95dad3008ff086f80096b39cb247144a
Arkadiusz Modzelewski <arcadius.modzelewski@gmail.com>
2023-06-27 21:17:48 +0200

    Data import function

--- churn_library.py
+++ churn_library.py
@@ -1,64 +1,76 @@
-# library doc string
+"""
+Prediction of Customer Churn
 
+Author: Arkadiusz Modzelewski
+"""
 
 # import libraries
 import os
-os.environ['QT_QPA_PLATFORM']='offscreen'
+import imgkit
+import pandas as pd
+from pandas.plotting import table
+from matplotlib import pyplot as plt
+
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
 
-
-def import_data(pth):
-    '''
+def import_data(pth: str) -> pd.DataFrame:
+    """
     returns dataframe for the csv found at pth
 
     input:
             pth: a path to the csv
     output:
-            df: pandas dataframe
-    '''	
-	pass
+            dataframe: pandas dataframe
+    """
+    dataframe = pd.read_csv(pth)
+    return dataframe
 
 
-def perform_eda(df):
-    '''
+def perform_eda(dataframe: pd.DataFrame, output_path: str) -> None:
+    """
     perform eda on df and save figures to images folder
     input:
-            df: pandas dataframe
+            dataframe: pandas dataframe
 
     output:
             None
-    '''
-	pass
-
-
-def encoder_helper(df, category_lst, response):
-    '''
-    helper function to turn each categorical column into a new column with
-    propotion of churn for each category - associated with cell 15 from the notebook
-
-    input:
-            df: pandas dataframe
-            category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be used for naming variables or index y column]
-
-    output:
-            df: pandas dataframe with new columns for
-    '''
+    """
     pass
 
 
-def perform_feature_engineering(df, response):
-    '''
+def encoder_helper(dataframe, category_lst, response):
+    """
+    helper function to turn each categorical column into a new column with
+    proportion of churn for each category - associated with cell 15 from the notebook
+
     input:
-              df: pandas dataframe
-              response: string of response name [optional argument that could be used for naming variables or index y column]
+            dataframe: pandas dataframe
+            category_lst: list of columns that contain categorical features
+            response: string of response name [optional argument that could be used
+                      for naming variables or index y column]
+
+    output:
+            dataframe: pandas dataframe with new columns for
+    """
+    pass
+
+
+def perform_feature_engineering(dataframe, response):
+    """
+    input:
+              dataframe: pandas dataframe
+              response: string of response name [optional argument that could be used
+                        for naming variables or index y column]
 
     output:
               X_train: X training data
               X_test: X testing data
               y_train: y training data
               y_test: y testing data
-    '''
+    """
+    pass
+
 
 def classification_report_image(y_train,
                                 y_test,
@@ -66,7 +78,7 @@ def classification_report_image(y_train,
                                 y_train_preds_rf,
                                 y_test_preds_lr,
                                 y_test_preds_rf):
-    '''
+    """
     produces classification report for training and testing results and stores report as image
     in images folder
     input:
@@ -79,12 +91,12 @@ def classification_report_image(y_train,
 
     output:
              None
-    '''
+    """
     pass
 
 
 def feature_importance_plot(model, X_data, output_pth):
-    '''
+    """
     creates and stores the feature importances in pth
     input:
             model: model object containing feature_importances_
@@ -93,11 +105,12 @@ def feature_importance_plot(model, X_data, output_pth):
 
     output:
              None
-    '''
+    """
     pass
 
+
 def train_models(X_train, X_test, y_train, y_test):
-    '''
+    """
     train, store model results: images + scores, and store models
     input:
               X_train: X training data
@@ -106,5 +119,9 @@ def train_models(X_train, X_test, y_train, y_test):
               y_test: y testing data
     output:
               None
-    '''
+    """
     pass
+
+
+if __name__ == '__main__':
+    df = import_data("data/bank_data.csv")
