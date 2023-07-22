c776c75d0121a0533a2092cfc08b888e7549c765
Arkadiusz Modzelewski <arcadius.modzelewski@gmail.com>
2023-07-22 18:42:22 +0200

    Add comments

--- churn_library.py
+++ churn_library.py
@@ -14,12 +14,13 @@ os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 if __name__ == '__main__':
 
     churn = ChurnModel(data_path="data/bank_data.csv")
+    # Import data for churn modelling
     churn.import_data()
-
+    # Perform EDA
     churn.perform_eda()
-
+    # Encoding categorical features
     churn.encode_cat_features(category_list=CAT_COLUMNS)
-
+    # Feature engineering and train test split
     churn.perform_feature_engineering(cols_to_keep=COLUMNS_TO_KEEP)
-
+    # Train logistic and random forest model and perform optimization and evaluation
     churn.train_models()
