9e53ce3e04b8405211588f8753bb53af4c48033a
Arkadiusz Modzelewski <arcadius.modzelewski@gmail.com>
2023-07-24 23:43:35 +0200

    Add unit tests for importing data

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,5 +1,10 @@
-import os
+"""
+Tests and logging for churn modelling methods and functions
+
+Author: Arkadiusz Modzelewski
+"""
 import logging
+import pytest
 import churn_model as cls
 
 logging.basicConfig(
@@ -9,24 +14,57 @@ logging.basicConfig(
     format='%(name)s - %(levelname)s - %(message)s')
 
 
-def test_import():
+# Fixture for importing data
+@pytest.fixture
+def imported_data():
+    # Code to import data using the import_data method
+    try:
+        churn = cls.ChurnModel()
+        churn.import_data("./data/bank_data.csv")
+        logging.info("Using import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Using import_eda: The file wasn't found")
+        raise err
+    return churn.dataframe
+
+
+def test_import_check_shape(imported_data):
     """
     Test data import - this example is completed for you to assist with the other test functions
     """
 
     try:
-        churn = cls.ChurnModel()
-        churn.import_data("./data/bank_data.csv")
-        logging.info("Testing import_data: SUCCESS")
-    except FileNotFoundError as err:
-        logging.error("Testing import_eda: The file wasn't found")
-        raise err
-
-    try:
-        assert churn.dataframe.shape[0] > 0
-        assert churn.dataframe.shape[1] > 0
+        assert imported_data.shape[0] > 0
+        assert imported_data.shape[1] > 0
     except AssertionError as err:
         logging.error("Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
+
+
+def test_import_check_target(imported_data):
+    """
+    Test data import - this example is completed for you to assist with the other test functions
+    """
+
+    try:
+        assert 'Churn' in list(imported_data.columns)
+    except AssertionError as err:
+        logging.error("Testing import_data:Target variable was not created")
+        raise err
+
+
+def test_import_check_dropped_cols(imported_data):
+    """
+    Test data import - this example is completed for you to assist with the other test functions
+    """
+
+    try:
+        assert 'Attrition_Flag' not in list(imported_data.columns)
+        assert 'Unnamed: 0' not in list(imported_data.columns)
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data:'Unnamed: 0' or 'Attrition_Flag' columns was not deleted"
+        )
         raise err
 
 
@@ -37,21 +75,15 @@ def test_import():
 #
 #
 # def test_encoder_helper(encoder_helper):
-#     '''
-# 	test encoder helper
-# 	'''
+#     """test encoder helper"""
 #
 #
 # def test_perform_feature_engineering(perform_feature_engineering):
-#     '''
-# 	test perform_feature_engineering
-# 	'''
+#     """test perform_feature_engineering"""
 #
 #
 # def test_train_models(train_models):
-#     '''
-# 	test train_models
-# 	'''
+#     """test train_models"""
 #
 #
 # if __name__ == "__main__":
